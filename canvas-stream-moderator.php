c67d0f14564be9905c4f3df6ac38595a02b6fba9
Daniel Künkel <Daniel.Kuenkel@informatik.hs-fulda.de>
2017-07-10 17:06:44 +0200

    - update streaming playground

--- canvas-stream-moderator.php
+++ canvas-stream-moderator.php
@@ -12,11 +12,14 @@
         <title>GestureNote</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="css/general.css">
         <link rel="stylesheet" href="css/study-preview.css">
         <link rel="stylesheet" href="css/gesture.css">
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+        <!--<link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/apdfllckaahabafndbhieahigkjlhalf">-->
+        <link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/ajhifddimkapgcifgcodmmfdlknahffk">
 
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
@@ -70,9 +73,12 @@
 
         <div class="btn-group">
             <button type="button" id="btn-start-screen-sharing" class="btn btn-info">Start Screensharing</button>
-            <button type="button" id="btn-stop-screen-sharing" class="btn btn-warning">Stop Sharingsharing</button>
+            <button type="button" id="btn-stop-screen-sharing" class="btn btn-warning">Stop Screensharing</button>
         </div>
-        
+
+        <button class="btn" onclick="openExtension('https://chrome.google.com/webstore/detail/ajhifddimkapgcifgcodmmfdlknahffk')" id="install-button">Add to Chrome</button>
+
+
         <div class="root" id="scenario" style="width: 100%;">
 
             <div style="position: absolute; width: 100%; height:auto;">
@@ -94,8 +100,10 @@
                 });
             });
 
+            var checkInterval;
             function onAllExternalsLoadedSuccessfully() {
                 console.log('all externals loaded');
+
 
                 var screen = new Screen('screen-unique-id'); // argument is optional
 
@@ -108,11 +116,148 @@
                     event.preventDefault();
                     screen.share();
                 });
-                
+
                 $('#btn-stop-screen-sharing').on('click', function (event) {
                     event.preventDefault();
                     screen.leave();
                 });
+            }
+
+
+//            function checkInstalledExtension() {
+//                console.log(chrome);
+////                var myPort = chrome.extension.connect('ajhifddimkapgcifgcodmmfdlknahffk', {test: 'test'});
+//                console.log('checkInstalledExtension');
+//                if (chrome.app.isInstalled) {
+//                    clearInterval(checkInterval);
+//                    document.getElementById('install-button').style.display = 'none';
+//                } else {
+//                    console.log('chrome extension not installed');
+//                }
+////                checkInterval = setTimeout(checkInstalledExtension(), 100000);
+//
+//
+//            }
+
+            function openExtension(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+
+            function checkScreenSharingExtension() {
+                var isChrome = !!navigator.webkitGetUserMedia;
+
+                // DetectRTC.js - https://github.com/muaz-khan/WebRTC-Experiment/tree/master/DetectRTC
+                // Below code is taken from RTCMultiConnection-v1.8.js (http://www.rtcmulticonnection.org/changes-log/#v1.8)
+                var DetectRTC = {};
+
+                (function () {
+
+                    var screenCallback;
+
+                    DetectRTC.screen = {
+                        chromeMediaSource: 'screen',
+                        getSourceId: function (callback) {
+                            if (!callback)
+                                throw '"callback" parameter is mandatory.';
+                            screenCallback = callback;
+                            window.postMessage('get-sourceId', '*');
+                        },
+                        isChromeExtensionAvailable: function (callback) {
+                            if (!callback)
+                                return;
+
+                            if (DetectRTC.screen.chromeMediaSource == 'desktop')
+                                return callback(true);
+
+                            // ask extension if it is available
+                            window.postMessage('are-you-there', '*');
+
+                            setTimeout(function () {
+                                if (DetectRTC.screen.chromeMediaSource == 'screen') {
+                                    callback(false);
+                                } else
+                                    callback(true);
+                            }, 2000);
+                        },
+                        onMessageCallback: function (data) {
+                            if (!(typeof data == 'string' || !!data.sourceId))
+                                return;
+
+                            console.log('chrome message', data);
+
+                            // "cancel" button is clicked
+                            if (data == 'PermissionDeniedError') {
+                                DetectRTC.screen.chromeMediaSource = 'PermissionDeniedError';
+                                if (screenCallback)
+                                    return screenCallback('PermissionDeniedError');
+                                else
+                                    throw new Error('PermissionDeniedError');
+                            }
+
+                            // extension notified his presence
+                            if (data == 'rtcmulticonnection-extension-loaded') {
+                                if (document.getElementById('install-button')) {
+                                    document.getElementById('install-button').parentNode.innerHTML = '<strong>Great!</strong> <a href="https://chrome.google.com/webstore/detail/screen-capturing/ajhifddimkapgcifgcodmmfdlknahffk" target="_blank">Google chrome extension</a> is installed.';
+                                }
+                                DetectRTC.screen.chromeMediaSource = 'desktop';
+                            }
+
+                            // extension shared temp sourceId
+                            if (data.sourceId) {
+                                DetectRTC.screen.sourceId = data.sourceId;
+                                if (screenCallback)
+                                    screenCallback(DetectRTC.screen.sourceId);
+                            }
+                        },
+                        getChromeExtensionStatus: function (callback) {
+                            if (!!navigator.mozGetUserMedia)
+                                return callback('not-chrome');
+
+                            var extensionid = 'ajhifddimkapgcifgcodmmfdlknahffk';
+
+                            var image = document.createElement('img');
+                            image.src = 'chrome-extension://' + extensionid + '/icon.png';
+                            image.onload = function () {
+                                DetectRTC.screen.chromeMediaSource = 'screen';
+                                window.postMessage('are-you-there', '*');
+                                setTimeout(function () {
+                                    if (!DetectRTC.screen.notInstalled) {
+                                        callback('installed-enabled');
+                                    }
+                                }, 2000);
+                            };
+                            image.onerror = function () {
+                                DetectRTC.screen.notInstalled = true;
+                                callback('not-installed');
+                            };
+                        }
+                    };
+
+                    // check if desktop-capture extension installed.
+                    if (window.postMessage && isChrome) {
+                        DetectRTC.screen.isChromeExtensionAvailable();
+                    }
+                })();
+
+                DetectRTC.screen.getChromeExtensionStatus(function (status) {
+                    if (status == 'installed-enabled') {
+                        if (document.getElementById('install-button')) {
+                            document.getElementById('install-button').parentNode.innerHTML = '<strong>Great!</strong> <a href="https://chrome.google.com/webstore/detail/screen-capturing/ajhifddimkapgcifgcodmmfdlknahffk" target="_blank">Google chrome extension</a> is installed.';
+                        }
+                        DetectRTC.screen.chromeMediaSource = 'desktop';
+                    }
+                });
+
+                window.addEventListener('message', function (event) {
+                    if (event.origin != window.location.origin) {
+                        return;
+                    }
+
+                    DetectRTC.screen.onMessageCallback(event.data);
+                });
+
+                console.log('current chromeMediaSource', DetectRTC.screen.chromeMediaSource);
             }
         </script>
     </body>
